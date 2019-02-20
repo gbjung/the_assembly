2c35dc6fe5e587f7bbe42a19724e258290545415
Gyeongbae Jung <gyeongbae.jj@gmail.com>
2019-02-20 00:43:31 -0500

    now can switch between dev and prod on manage.py

--- manage.py
+++ manage.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
 import os
 import sys
+import environ
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.dev")
+    env = environ.Env(DEBUG=(bool, False),)
+    environ.Env.read_env('settings/.env')
 
+    if env('ENV_SETTING') == 'dev':
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.dev")
+    else:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "production.dev")
+        
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
