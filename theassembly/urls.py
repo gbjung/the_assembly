703931e3e238de3ff056d083788fa0d90ff4d6da
Gyeongbae Jung <gyeongbae.jj@gmail.com>
2019-02-20 18:43:09 -0500

    aaaa

--- theassembly/urls.py
+++ theassembly/urls.py
@@ -1,6 +1,8 @@
 from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
@@ -18,15 +20,13 @@ urlpatterns = [
     url(r'^subscriptions/', include(subscription_urls)),
 ]
 
-if settings.DEBUG:
-    from django.conf.urls.static import static
-    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+if settings.DEV:
     import debug_toolbar
     urlpatterns = [
         url(r'^__debug__/', include(debug_toolbar.urls)),
     ] + urlpatterns
 # Serve static and media files from development server
-    urlpatterns += staticfiles_urlpatterns()
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += [url(r'', include(wagtail_urls))]
