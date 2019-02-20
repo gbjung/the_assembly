6c71e66d84a8506b0a060c7a6409697cb0d4ea02
Gyeongbae Jung <gyeongbae.jj@gmail.com>
2019-02-20 01:43:07 -0500

    prod settings

--- settings/production.py
+++ settings/production.py
@@ -1,11 +1,27 @@
 from .base import *
 
 DEBUG = False
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env("SECRET_KEY")
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = [env("PROD_IP")]
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = env("EMAIL_PORT")
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env("DB_NAME"),
+        'USER': env("DB_USER"),
+        'PASSWORD': env("DB_PASSWORD"),
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 try:
