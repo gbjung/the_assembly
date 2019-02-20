2111598f7dfe078fb5a4fce80eab8fd354584db3
Gyeongbae Jung <gyeongbae.jj@gmail.com>
2019-02-20 01:42:57 -0500

    changed newsletters to use send_mass_newsletter using the actual email creds. Also fixed url pathing

--- newsletters/views.py
+++ newsletters/views.py
@@ -1,4 +1,4 @@
-from django.core.mail import send_mail
+from django.core.mail import get_connection, EmailMultiAlternatives
 from django.template.loader import render_to_string
 from django.views.generic.base import TemplateView
 from wagtail.core.signals import page_published
@@ -14,24 +14,48 @@ class NewsletterMakerView(TemplateView):
         context = super().get_context_data(**kwargs)
         return context
 
+def send_mass_html_mail(datatuple):
+    """
+    Given a datatuple of (subject, text_content, html_content, from_email,
+    recipient_list), sends each message to each recipient list. Returns the
+    number of emails sent.
+
+    If from_email is None, the DEFAULT_FROM_EMAIL setting is used.
+    If auth_user and auth_password are set, they're used to log in.
+    If auth_user is None, the EMAIL_HOST_USER setting is used.
+    If auth_password is None, the EMAIL_HOST_PASSWORD setting is used.
+
+    """
+    connection = get_connection()
+    messages = []
+    for subject, text, html, from_email, recipient in datatuple:
+        message = EmailMultiAlternatives(subject, text, from_email, recipient)
+        message.attach_alternative(html, 'text/html')
+        messages.append(message)
+
+    return connection.send_messages(messages)
+
 def send_newsletters(sender, **kwargs):
     """
     When a newsletter is published, trigger an email send
     to all Subscribers.
     """
+    connection = get_connection()
     msg_html = render_to_string('newsletters/newsletter.html',
                                 {'newsletter_html': kwargs['instance'].html})
-                                
-    active_subscribers = Subscriber.objects.filter(active=True)
+
+    active_subscribers = Subscriber.objects.filter(is_active=True)
+    emails = []
+    subject = kwargs['instance'].subject
+    text = None
+    from_email = 'newsletter@theassembly.xyz'
+
     for subscriber in active_subscribers.iterator():
-        send_mail(
-            kwargs['instance'].subject,
-            'Here is the message.',
-            'gyeongbae.jj',
-            ['gyeongbae.jj@gmail.com'],
-            fail_silently=False,
-            html_message=msg_html,
-        )
-    return
+        email = EmailMultiAlternatives(subject, text, from_email, [subscriber.email])
+        email.attach_alternative(msg_html, 'text/html')
+        emails.append(email)
+
+    return connection.send_messages(emails)
+
 
 page_published.connect(send_newsletters, sender=Newsletter)
--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from subscriptions.views import create_new_subscriber
 
 urlpatterns = [
-    url(r'^new/?', create_new_subscriber, name="newsletter_maker"),
+    url(r'^new/?', create_new_subscriber),
 ]
